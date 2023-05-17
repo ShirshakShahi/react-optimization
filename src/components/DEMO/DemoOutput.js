2415f18e14d427c118691cddd74892ceb48d77fa
ShirshakShahi <royalshirshal000@gmail.com>
2023-05-17 22:29:20 +0545

    hi

--- src/components/DEMO/DemoOutput.js
+++ src/components/DEMO/DemoOutput.js
@@ -3,7 +3,7 @@ import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
     console.log("demo output is running ");
-    return <MyParagraph>{props.show ? "this is new" : ""}</MyParagraph>
+    return <MyParagraph>{props.show ? "this is new!!!" : ""}</MyParagraph>
 }
 
 export default React.memo(DemoOutput);
--- src/components/DEMO/MyParagraph.js
+++ src/components/DEMO/MyParagraph.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MyParagraph = (props) => {
     console.log("MyParagraph output is running ");
-    return <p>props.children</p>
+    return <p>{props.children}</p>
 }
 
 export default MyParagraph;
